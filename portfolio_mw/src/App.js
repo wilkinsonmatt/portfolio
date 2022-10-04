5a3a300d52f9459491d440bf6b1f85e2f8b18502
wilkinsonmatt <wilkinsonmatt01@gmail.com>
2022-10-04 11:26:52 -0700

    padding

--- portfolio_mw/src/App.js
+++ portfolio_mw/src/App.js
@@ -1,10 +1,10 @@
 import React  from "react";
 import Navbar from "./Components/Navbar/Navbar";
+import './App.css'
 
 function App(){
   return (
     <React.Fragment>
-      test
      <Navbar />
     </React.Fragment>
   );
