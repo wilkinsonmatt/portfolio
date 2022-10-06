4a4fb840c6f09f3cb59ddff86cb617a94c680ee5
wilkinsonmatt <wilkinsonmatt01@gmail.com>
2022-10-06 00:29:47 -0700

    moved home page around

--- portfolio_mw/src/App.js
+++ portfolio_mw/src/App.js
@@ -1,13 +1,16 @@
 import React  from "react";
 import { Routes, Route } from 'react-router-dom';
+import Home from './Components/Home';
 import Layout from './Components/Layout';
-import './App.scss'
+import './App.scss';
 
 function App(){
   return (
     <>
       <Routes>
-        <Route path="/" element={<Layout />} />
+        <Route path="/" element={<Layout />}>
+          <Route index element={<Home />} />
+        </Route>
       </Routes>
     </>
   );
---
+++ portfolio_mw/src/Components/Home/index.js
@@ -0,0 +1,16 @@
+const Home = () => { 
+
+  return (
+    <div classsName = "container home-page">
+      <div className="text-zone">
+      
+      </div>
+    </div>
+
+
+
+
+  );
+};
+
+export default Home
