77a1522ad8a6fcdaca6a7fe4cffc9cedf22e1c61
wilkinsonmatt <wilkinsonmatt01@gmail.com>
2022-10-05 22:34:14 -0700

    nav bar fixed

--- portfolio_mw/src/Components/Sidebar/index.js
+++ portfolio_mw/src/Components/Sidebar/index.js
@@ -2,8 +2,8 @@ import './index.scss';
 import { Link, NavLink } from 'react-router-dom';
 import LogoS from '../../assets/images/logo-s.png';
 import LogoSubtitle from '../../assets/images/logo_sub.png';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faHome } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faHome, faUser, faEnvelope } from '@fortawesome/free-solid-svg-icons';
 
 
 const Sidebar = () => (
@@ -15,6 +15,16 @@ const Sidebar = () => (
     <nav>
       <NavLink exact="true" activeclassname="active" to="/">
         <FontAwesomeIcon icon={faHome} color="#4d4d4e" />
+      </NavLink>
+    </nav>
+    <nav>
+      <NavLink exact="true" activeclassname="active" className="about-link" to="/about">
+        <FontAwesomeIcon icon={faUser} color="#4d4d4e" />
+      </NavLink>
+    </nav>
+    <nav>
+      <NavLink exact="true" activeclassname="active" className="contact-link" to="/contact">
+        <FontAwesomeIcon icon={faEnvelope} color="#4d4d4e" />
       </NavLink>
     </nav>
   </div>
