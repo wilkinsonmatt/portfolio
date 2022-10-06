7b54a72e15820935b93ff048e169c9b1a335cbf0
wilkinsonmatt <wilkinsonmatt01@gmail.com>
2022-10-06 01:15:42 -0700

    Can't get home page screen to work

--- portfolio_mw/src/Components/Home/index.js
+++ portfolio_mw/src/Components/Home/index.js
@@ -1,15 +1,16 @@
+import img1 from '../../assets/pictures/img4.jpg';
+import { Link,  } from 'react-router-dom';
+
+
 const Home = () => { 
 
   return (
-    <div classsName = "container home-page">
-      <div className="text-zone">
-      
-      </div>
+    <div classsName = "home-page">
+      <Link className='logo' to='/'>
+        {/* <img src={img1} alt="logo" /> */}
+        <p>I'm Full Stack Developer in <span class="typed" data-typed-items="JavaScript, React, C#, .NET"></span></p>
+      </Link>
     </div>
-
-
-
-
   );
 };
 
--- portfolio_mw/src/Components/Sidebar/index.js
+++ portfolio_mw/src/Components/Sidebar/index.js
@@ -2,6 +2,7 @@ import './index.scss';
 import { Link, NavLink } from 'react-router-dom';
 import LogoS from '../../assets/images/logo-s.png';
 import LogoSubtitle from '../../assets/images/logo_sub.png';
+import img1 from '../../assets/pictures/img2.png';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faHome, faUser, faEnvelope, faFilePdf } from '@fortawesome/free-solid-svg-icons';
 import { faLinkedin, faGithub } from '@fortawesome/free-brands-svg-icons'
@@ -10,7 +11,7 @@ import { faLinkedin, faGithub } from '@fortawesome/free-brands-svg-icons'
 const Sidebar = () => (
   <div className='nav-bar'>
     <Link className='logo' to='/'>
-        <img src={LogoS} alt="logo" />
+        <img src={img1} alt="logo" />
         <img className="sub-logo" src={LogoSubtitle} alt="slobodan" />
     </Link>
     <nav>
