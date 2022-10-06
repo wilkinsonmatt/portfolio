80fa9f176eff22fd89fba3044d3397f2989e35ff
wilkinsonmatt <wilkinsonmatt01@gmail.com>
2022-10-06 07:31:41 -0700

    font for home page

--- portfolio_mw/src/Components/Home/index.js
+++ portfolio_mw/src/Components/Home/index.js
@@ -5,13 +5,21 @@ import { Link,  } from 'react-router-dom';
 const Home = () => { 
 
   return (
-    <div classsName = "home-page">
+    <div classsName = "container home-page">
       <Link className='logo' to='/'>
-        {/* <img src={img1} alt="logo" /> */}
+        
+        I'm Full Stack Developer in
         <p>I'm Full Stack Developer in <span class="typed" data-typed-items="JavaScript, React, C#, .NET"></span></p>
+      lobodan
       </Link>
+      web
     </div>
   );
 };
 
 export default Home
+
+
+
+
+{/* <img src={img1} alt="logo" /> */}
