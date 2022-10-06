1c4b185640a485bcda37cac8d31d9f5f8164e83e
wilkinsonmatt <wilkinsonmatt01@gmail.com>
2022-10-05 23:32:23 -0700

    font awesome resume icon

--- portfolio_mw/src/Components/Sidebar/index.js
+++ portfolio_mw/src/Components/Sidebar/index.js
@@ -3,8 +3,8 @@ import { Link, NavLink } from 'react-router-dom';
 import LogoS from '../../assets/images/logo-s.png';
 import LogoSubtitle from '../../assets/images/logo_sub.png';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faHome, faUser, faEnvelope } from '@fortawesome/free-solid-svg-icons';
-import { faLinkedin, faGithub, faYoutube, faSkype } from '@fortawesome/free-brands-svg-icons'
+import { faHome, faUser, faEnvelope, faFilePdf } from '@fortawesome/free-solid-svg-icons';
+import { faLinkedin, faGithub } from '@fortawesome/free-brands-svg-icons'
 
 
 const Sidebar = () => (
@@ -44,7 +44,16 @@ const Sidebar = () => (
         rel='noreferrer' 
         href='https://github.com/wilkinsonmatt'
         >
-          <FontAwesomeIcon icon={faLinkedin} color="#4d4d4e" />
+          <FontAwesomeIcon icon={faGithub} color="#4d4d4e" />
+        </a>
+      </li>
+      <li>
+        <a 
+        target="_blank" 
+        rel='noreferrer' 
+        href='https://github.com/wilkinsonmatt'
+        >
+          <FontAwesomeIcon icon={faFilePdf} color="#4d4d4e" />
         </a>
       </li>
     </ul>
