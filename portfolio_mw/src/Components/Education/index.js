50500ec4ebb5d02b48ef8996579b5932612d223d
wilkinsonmatt <wilkinsonmatt01@gmail.com>
2022-10-13 20:56:43 -0700

    more education

--- portfolio_mw/src/Components/Education/index.js
+++ portfolio_mw/src/Components/Education/index.js
@@ -7,6 +7,25 @@ const Education = () => {
       <div className="text-zone">
         <h1><strong>Education</strong></h1>
 
+
+          <div class="box2">
+      <h2>Education:</h2>
+      <ul>    
+        <P>Bachelor of Science: Health Management & Policy</P>
+        <P>Oregon State University — 2015</P>
+      </ul>
+      <h2>Job Experience:</h2>
+      <Ul>
+        <h3>Computer User Support Analyst 2 (EPIC Support)</h3>
+        <ul>
+          <p>September 2019 to January 2020</p>
+          <p>OHSU — Portland, Oregon</p>
+          <li>Provided technical support, diagnostic help, and operating instructions for the EPIC. Supports all departments of OHSU</li>
+          <li>Provided patient technical support for MyChart</li>
+          <li>Created tickets for the analyst teams when troubleshooting was unsuccessful</li>
+        </ul>
+      </Ul>
+    </div>
         <p>
           coming soon
         </p>
